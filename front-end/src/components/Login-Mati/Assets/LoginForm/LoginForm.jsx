b2d329a19bf72146789544b6ba60e6b2cf6d7424
mriettas <mriettas@correo.uss.cl>
2024-12-03 00:03:57 -0300

    update del pop up

--- front-end/src/components/Login-Mati/Assets/LoginForm/LoginForm.jsx
+++ front-end/src/components/Login-Mati/Assets/LoginForm/LoginForm.jsx
@@ -3,6 +3,8 @@ import './LoginForm.css';
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { fetchUserInfo } from '@services/getUserInfo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const LoginForm = () => {
   const [rut, setRut] = useState('');
@@ -35,70 +37,114 @@ export const LoginForm = () => {
           const currentUser = userInfo.find((user) => user.rut === rut);
           const userType = currentUser?.tipousuario;
 
-          // Guardar la información del usuario en el contexto
+          // Mostrar notificación de éxito
+          toast.success('Inicio de sesión exitoso. Redirigiendo...', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          });
 
           // Redirigir según el tipo de usuario
-          if (userType === 'supervisor') {
-            window.location.href = '/dashboard';
-          } else if (userType === 'medico') {
-            window.location.href = '/form';
-          } else if (userType === 'administrativo') {
-            window.location.href = '/formulario-registro';
-          } else {
-            setError('Tipo de usuario no reconocido.');
-          }
-        }catch(userInfoError) {
+          setTimeout(() => {
+            if (userType === 'supervisor') {
+              window.location.href = '/dashboard';
+            } else if (userType === 'medico') {
+              window.location.href = '/form';
+            } else if (userType === 'administrativo') {
+              window.location.href = '/formulario-registro';
+            } else {
+              setError('Tipo de usuario no reconocido.');
+              toast.error('Tipo de usuario no reconocido.', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+              });
+            }
+          }, 3000);
+        } catch (userInfoError) {
           setError('Error al obtener la información del usuario.');
-          console.error('Error en fetchUserInfo', userInfoError)
+          toast.error('Error al obtener la información del usuario.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          });
+          console.error('Error en fetchUserInfo', userInfoError);
         }
       } else {
         setError('Usuario o contraseña incorrectos.');
+        toast.error('Usuario o contraseña incorrectos.', {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
       }
     } catch (e) {
-      setError('Error al conectar con el servidor.', e);
+      setError('Error al conectar con el servidor.');
+      toast.error('Error al conectar con el servidor.', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
     }
   };
 
   return (
     <div className='login-page'>
-    <div className='login-wrapper'>
-      <form onSubmit={handleSubmit}>
-        <img
-          src="https://b2597467.smushcdn.com/2597467/wp-content/uploads/logo-redsalud-dental-blanco.png?lossy=1&strip=1&webp=1"
-          alt="LogoLogin"
-          className="LogoLogin"
-        />
-        <div className='input-box'>
-          <input
-            type="text"
-            placeholder='Usuario'
-            required
-            value={rut}
-            onChange={(e) => setRut(e.target.value)} 
+      <div className='login-wrapper'>
+        <form onSubmit={handleSubmit}>
+          <img
+            src="https://b2597467.smushcdn.com/2597467/wp-content/uploads/logo-redsalud-dental-blanco.png?lossy=1&strip=1&webp=1"
+            alt="LogoLogin"
+            className="LogoLogin"
           />
-          <FaUser className='icon' />
-        </div>
-        <div className='input-box mt-2'>
-          <input
-            type="password"
-            placeholder='Contraseña'
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <RiLockPasswordFill className='icon' />
-        </div>
+          <div className='input-box'>
+            <input
+              type="text"
+              placeholder='Usuario'
+              required
+              value={rut}
+              onChange={(e) => setRut(e.target.value)} 
+            />
+            <FaUser className='icon' />
+          </div>
+          <div className='input-box mt-2'>
+            <input
+              type="password"
+              placeholder='Contraseña'
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <RiLockPasswordFill className='icon' />
+          </div>
 
-        <div className='remember-forgot'>
-          <label><input type="checkbox" /> Recordarme</label>
-          <a href="/reset-password">Olvidé mi contraseña</a>
-        </div>
+          <div className='remember-forgot'>
+            <label><input type="checkbox" /> Recordarme</label>
+            <a href="/reset-password">Olvidé mi contraseña</a>
+          </div>
 
-        {error && <p className="error-message">{error}</p>}
-        
-        <button type="submit">Ingresar</button>
-      </form>
-    </div>
+          {error && <p className="error-message">{error}</p>}
+          
+          <button type="submit">Ingresar</button>
+        </form>
+      </div>
+      {/* Contenedor de notificaciones */}
+      <ToastContainer />
     </div>
   );
 };
